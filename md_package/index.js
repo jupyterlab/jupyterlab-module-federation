0fbd586c38daa46f7423ac1b77cf2a8a2f71e5e4
Steven Silvester <stslve@amazon.com>
2020-07-22 14:19:54 -0500

    do not depend on middle package yet

--- md_package/index.js
+++ md_package/index.js
@@ -6,7 +6,6 @@ import { MarkdownViewer, MarkdownViewerFactory, IMarkdownViewerTracker } from '@
 import { IRenderMimeRegistry, markdownRendererFactory } from '@jupyterlab/rendermime';
 import { ISettingRegistry } from '@jupyterlab/settingregistry';
 import { PathExt } from '@jupyterlab/coreutils';
-import { IMiddleToken } from '@jupyterlab/example-federated-middle';
 /**
  * The command IDs used by the markdownviewer plugin.
  */
@@ -26,7 +25,7 @@ const plugin = {
     activate,
     id: '@jupyterlab/markdownviewer-extension:plugin',
     provides: IMarkdownViewerTracker,
-    requires: [ILayoutRestorer, IRenderMimeRegistry, ISettingRegistry, IMiddleToken],
+    requires: [ILayoutRestorer, IRenderMimeRegistry, ISettingRegistry],
     autoStart: true
 };
 /**
@@ -34,8 +33,6 @@ const plugin = {
  */
 function activate(app, restorer, rendermime, settingRegistry, middleToken) {
     const { commands, docRegistry } = app;
-
-    console.log('middleToken', middleToken);
 
     // Add the markdown renderer factory.
     rendermime.addFactory(markdownRendererFactory);

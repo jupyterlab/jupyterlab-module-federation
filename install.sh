a5150ce962f54091d6133008773301846cf19698
Steven Silvester <stslve@amazon.com>
2020-08-01 04:56:06 -0500

    install relative paths

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ set -o pipefail
 pip install setuptools pip --upgrade
 pip install -v -e ".[test]"
 jlpm && jlpm run build
-pip install md_package
-pip install middle_package
-pip install theme_package
+pip install ./md_package
+pip install ./middle_package
+pip install ./theme_package
 python labextensions.py develop md_package
 python labextensions.py develop middle_package
 python labextensions.py develop theme_package

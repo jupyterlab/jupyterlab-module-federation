9def562c39238d8451965ba0295460b232896795
Steven Silvester <stslve@amazon.com>
2020-08-10 15:20:37 -0500

    use released version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='jupyterlab-module-federation',
       version='0.1.0',
       py_modules = ['main'],
       install_requires=[
-        'jupyterlab==3.0.0a7',
+        'jupyterlab==3.0.0a8',
         'jupyterlab_server==2.0.0a4'
     ],
     extras_require={

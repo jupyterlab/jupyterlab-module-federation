579443aaedb02ae0bdc6a55cab212abdeb959e8f
Steven Silvester <stslve@amazon.com>
2020-08-11 20:38:59 -0500

    bump python deps

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(name='jupyterlab-module-federation',
       version='0.1.0',
       py_modules = ['main'],
       install_requires=[
-        'jupyterlab==3.0.0a8',
-        'jupyterlab_server==2.0.0a4'
+        'jupyterlab==3.0.0a9',
+        'jupyterlab_server==2.0.0b0'
     ],
     extras_require={
         'test': ['pytest', 'coverage']

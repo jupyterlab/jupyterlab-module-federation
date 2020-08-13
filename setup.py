4c79c11c7dfd01a3fb34441c451ae3385372b09f
Jason Grout <jgrout6@bloomberg.net>
2020-08-12 22:34:00 -0700

    Update jlab dependency to alpha 10

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='jupyterlab-module-federation',
       version='0.1.0',
       py_modules = ['main'],
       install_requires=[
-        'jupyterlab==3.0.0a9'
+        'jupyterlab==3.0.0a10'
     ],
     extras_require={
         'test': ['pytest', 'coverage']
